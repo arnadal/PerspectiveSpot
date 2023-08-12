581eacc30ced46140a056bc320d3326f6e0c1864
AR Nadal Verma <arnadal2010@gmail.com>
2023-08-12 13:51:30 +0530

    Initial
    The first commit to the main branch.

---
+++ Index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmC3oT__m1QwQtZZGUJug9Nq0Mk-G65FQ",
+  authDomain: "perspectivespot-app.firebaseapp.com",
+  databaseURL: "https://perspectivespot-app-default-rtdb.firebaseio.com",
+  projectId: "perspectivespot-app",
+  storageBucket: "perspectivespot-app.appspot.com",
+  messagingSenderId: "61597461758",
+  appId: "1:61597461758:web:47c8ca192687901d2149af",
+  measurementId: "G-5F26F460WE"
+};
