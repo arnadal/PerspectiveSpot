9d3280c2b7d2570f754633f98dbb7273d1219b07
AR Nadal Verma <arnadal2010@gmail.com>
2023-08-16 17:14:25 +0530

    Finishing the Finished!
    Finishing touches!

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyBmC3oT__m1QwQtZZGUJug9Nq0Mk-G65FQ",
   authDomain: "perspectivespot-app.firebaseapp.com",
   databaseURL: "https://perspectivespot-app-default-rtdb.firebaseio.com",
@@ -7,4 +7,33 @@ const firebaseConfig = {
   messagingSenderId: "61597461758",
   appId: "1:61597461758:web:47c8ca192687901d2149af",
   measurementId: "G-5F26F460WE"
+});
+
+Database = firebase.database();
+
+Inputs = {
+
+  Name: document.querySelector(`#Name`),
+  Comment: document.querySelector(`#Comment`)
+
+};
+
+function CreatePost() {
+
+  const NewPostKey = Database.ref(`comments/`).push().key;
+
+  Database.ref(`comments/${NewPostKey}`).set({
+
+    Name: Inputs.Name.value,
+    Comment: Inputs.Comment.value,
+    Timestamp: Date.now()
+
+  });
+
+};
+
+function RenderPost(Data) {
+
+
+
 };
