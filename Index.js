346613aee0602df8de6ca067470abec5af9660a3
AR Nadal Verma <arnadal2010@gmail.com>
2023-08-17 00:11:26 +0530

    Completing the Completion of the Incomplete!
    Completing the Completion of the Incomplete!

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+/** Initialize the Firebase application and connect to the Firebase project. */
+
 firebase.initializeApp({
   apiKey: "AIzaSyBmC3oT__m1QwQtZZGUJug9Nq0Mk-G65FQ",
   authDomain: "perspectivespot-app.firebaseapp.com",
@@ -9,22 +11,56 @@ firebase.initializeApp({
   measurementId: "G-5F26F460WE"
 });
 
+/** A global-scope variable "Database" that is a container for database manipulation of Firebase. */
+
 Database = firebase.database();
+
+/** An object that contains all inputs on the page. */
 
 Inputs = {
 
   Name: document.querySelector(`#Name`),
+  Email: document.querySelector(`#Email`),
+  ContactNumber: document.querySelector(`#ContactNumber`),
   Comment: document.querySelector(`#Comment`)
 
 };
 
+// Listen to the database if any child is added.
+
+Database.ref(`Comments/`).on(`child_added`, Snap => {
+
+  RenderPost({
+
+    Name: Snap.child(`Name`).val(),
+    Email: Snap.child(`Email`).val(),
+    ContactNumber: Snap.child(`Email`).val(),
+    Comment: Snap.child(`Comment`).val(),
+    Timestamp: Snap.child(`Timestamp`).val()
+
+  });
+
+});
+
+
+/** A function that sends user-generated information from inputs as a request to the server to be appended into the database. */
+
 function CreatePost() {
 
-  const NewPostKey = Database.ref(`comments/`).push().key;
+  if (
+    !Inputs.Name.value ||
+    !Inputs.Email.value ||
+    !Inputs.ContactNumber.value ||
+    !Inputs.Comment.value
+  ) return InvalidPost();
 
-  Database.ref(`comments/${NewPostKey}`).set({
+  const NewPostKey = Database.ref(`Comments/`).push().key;
+
+  Database.ref(`Comments/${NewPostKey}`).set({
 
     Name: Inputs.Name.value,
+    Email: Inputs.Email.value,
+    ContactNumber: Inputs.ContactNumber.value,
     Comment: Inputs.Comment.value,
     Timestamp: Date.now()
 
@@ -32,8 +68,60 @@ function CreatePost() {
 
 };
 
-function RenderPost(Data) {
+/** A function that marks the user-generated post invalid. */
 
+function InvalidPost() {
 
+  document.querySelector(`div#InvalidPostMessage`).style.display = `block`;
+
+};
+
+/** A function that renders posts. */
+
+function RenderPost(Data = {}) {
+
+  let NewElement = document.createElement(`div`);
+  NewElement.className = `Perspective`;
+  NewElement.innerHTML = `
+
+  <small style="float: right;">${TranslateDate(Data.Timestamp)}</small>
+  <label>${Data.Name}</label>
+  <p>${Data.Comment}</p>
+
+  `;
+
+  document.querySelector(`#PerspectiveList`).appendChild(NewElement);
+  document.querySelector(`#NoPerspectiveFoundMessage`).style.display = `none`;
+
+};
+
+/** A function that translates dates into human-readable text. */
+
+function TranslateDate(Timestamp) {
+
+  let MonthList = [`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`];
+
+  function Pad60(Integer) {
+
+    let ParsedInteger = Integer;
+
+    if (Integer < 10) ParsedInteger = `0${Integer}`;
+
+    return ParsedInteger;
+
+  };
+
+  let InterprettedDate = new Date(Timestamp);
+
+  let Year = InterprettedDate.getFullYear();
+  let Month = MonthList[InterprettedDate.getMonth()];
+  let DayOfMonth = InterprettedDate.getDate();
+  let Hour = Pad60(InterprettedDate.getHours());
+  let Minute = Pad60(InterprettedDate.getMinutes());
+  let Second = Pad60(InterprettedDate.getSeconds());
+
+  let RenderedDate = `${Hour}:${Minute}:${Second}, ${DayOfMonth} ${Month} ${Year}`;
+
+  return RenderedDate;
 
 };
